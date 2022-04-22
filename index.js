360e869364007d691d51f724ede8f39c064cf885
Tho Nguyen <tho.nguyen@transwap.com>
2022-04-22 16:59:59 +0700

    commit: create v1.1.1

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ module.exports = {
   log,
 };
 
+var apiKey = "123abc";
 function log() {
   console.log("This is a simple module to log");
   console.log("This is v2");
+  console.log("v3 with apikey");
+  console.log(apiKey);
 }
